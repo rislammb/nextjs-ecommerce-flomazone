78188cf2af0c70e8790eaa75c9ca3b09005b4940
unknown <rislammb@gmail.com>
2024-04-13 07:54:57 +0600

    add product page with add to cart and necessaries db functionality, add not-found page, add loading page

--- src/components/PriceTag.tsx
+++ src/components/PriceTag.tsx
@@ -6,5 +6,5 @@ interface PriceTagProps {
 }
 
 export default function PriceTag({ price, className }: PriceTagProps) {
-  return <span className={`${className}`}>{formatPrice(price)}</span>;
+  return <span className={`badge ${className}`}>{formatPrice(price)}</span>;
 }
