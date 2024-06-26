687e4f612e98710c3ed3105b5f28d10b3040bb8e
unknown <rislammb@gmail.com>
2024-06-26 13:32:05 +0600

    update next-auth import

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,9 +2,8 @@ import { mergeAnonymousCartIntoUserCart } from "@/lib/db/cart";
 import prisma from "@/lib/db/prisma";
 import { env } from "@/lib/env";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { NextAuthOptions } from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
-import NextAuth from "next-auth/next";
 import Google from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
